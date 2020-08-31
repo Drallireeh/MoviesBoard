c8cba92513b81f923ba6f4065e456c4cca45e490
kenny herillard <Herillard>
2020-08-31 14:48:31 +0200

    First version html/css of movie's card

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,10 +1,17 @@
 import React from 'react';
-// import './Movie.css';
+import './Movies.css';
 
 const Movie = (props) => {
 	return (
-		<div className="card card full-image">
-			{props.movie.id}
+		<div className="movie-cnt col-md-4 col-sm-12">
+            <h2>{props.movie.title}</h2>
+			<img className="wrapper" src={props.movie.poster !== "" ? props.movie.poster : props.movie.backdrop}/>
+            <p>{props.movie.description}</p>
+            <span>Sortit le {props.movie.release_date}</span>
+            <div>
+                <button className="btn btn-primary">Ajouter</button>
+                <button className="btn btn-primary">Supprimer</button>
+            </div>
 		</div>
 	);
 };
