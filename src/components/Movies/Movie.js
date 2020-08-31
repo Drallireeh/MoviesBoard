09a3744839558837f47d56a7eab96dacd47db9ef
kenny herillard <Herillard>
2020-08-31 16:17:18 +0200

    Ajout de la redirection au clic sur un film et ajout d'un component pour ajouter des films

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Movies.css';
+import { Link } from "react-router-dom";
 
 const Movie = (props) => {
 	return (
-		<div className="movie-cnt col-md-4 col-sm-12">
-            <h2>{props.movie.title}</h2>
-			<img className="wrapper" src={props.movie.poster !== "" ? props.movie.poster : props.movie.backdrop}/>
-            <p>{props.movie.description}</p>
-            <span>Sortit le {props.movie.release_date}</span>
+		<div className="movie-cnt col-md-4 col-sm-12" to={"/" + props.movie.id}>
+            <Link className="link-movie" to={"/" + props.movie.id}>
+                <h2>{props.movie.title}</h2>
+                <img className="wrapper" src={props.movie.poster !== "" ? props.movie.poster : props.movie.backdrop} />
+                <p>{props.movie.description}</p>
+                <span>Sortit le {props.movie.release_date}</span>
+            </Link>
             <div>
-                <button className="btn btn-success">Ajouter</button>
+                <button className="btn btn-success">Modifier</button>
                 <button className="btn btn-danger">Supprimer</button>
             </div>
 		</div>
